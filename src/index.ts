78707a221fabf570d8f991f30d4c8b47315300e4
xjchenhao <wy.chenhao@qq.com>
2018-12-27 15:50:02 +0800

    使用自己封装的支持async的crawler模块

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
+'use strict';
+
+const Crawler = require("async-crawler");
+const co = require('co');
+
+const crawler = new Crawler();
+
+co(function* () {
+
+    const result = yield crawler.asyncDirect({
+        uri: 'http://www.nhl-pharm.com/news.html',
+        callback: function (error: any, res: any) {
+            if (error) {
+                console.log(error);
+            } else {
+                var $ = res.$;
+                const $newscenterA = $('#newscenter a');
+                const rusult: Array<object> = [];
+
+                for (let i = 0, l = $newscenterA.length; i < l; i++) {
+                    const item = $newscenterA[i];
+                    rusult.push({
+                        title: $(item).text(),
+                        href: $(item).attr('href'),
+                    })
+                }
+
+                return rusult;
+            }
+        }
+    });
+
+    console.log(result);
 
 
-const sayHello = function (person: string) {
-    return 'Hello, ' + person;
-}
-
-const user: string = 'Tom';
-console.log(sayHello(user));
+});
